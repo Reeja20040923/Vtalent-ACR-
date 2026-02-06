a32dc327a5b2209ab85ed83d8db4a94391535dc6
Reeja20040923 <reejayaramalla@gmail.com>
2026-02-06 14:26:01 +0530

    Feb2-6

---
+++ src/com/vtalent/Sum.java
@@ -0,0 +1,12 @@
+package com.vtalent;
+
+public class Sum {
+    public static void main(String[] args){
+        int n = 20;
+        int count = 0;
+        for(int i=0;i<=n;i++){
+            count++;
+        }
+        System.out.println("Sum of first 20 numbers is:" + count);
+    }
+}
