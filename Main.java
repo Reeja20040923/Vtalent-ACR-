aa420d210e87f6fe5a07efec5a62144a7a7983b2
Yaramalla Reeja <reejayaramalla@gmail.com>
2026-02-26 22:08:55 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,8 @@
-package Vtalent.com.Feb;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "Reeja"
 
-import java.util.Scanner;
-
-//implements exception handling in java
-class InvalidMarkException extends Exception{
-    InvalidMarkException(String message){
-        super(message);
     }
-}
-class StudentMarks{
-    void validateMarks(int marks[]) throws InvalidMarkException{
-        for(int i=0;i<marks.length;i++){
-            if(marks[i]<0||marks[i]>100){
-                throw new InvalidMarkException("Error: Invalid Mark Found");
-            }
-        }
-        System.out.println("Valid Marks");
-    }
-}
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int marks[] = new int[n];
-        for(int i=0;i<n;i++){
-            marks[i] = sc.nextInt();
-        }
-        StudentMarks sm = new StudentMarks();
-        try{
-            sm.validateMarks(marks);
-        }
-        catch(InvalidMarkException e){
-            System.out.println(e.getMessage());
-        }
-        sc.close();
-    }
-
 }
