5e8e79bcd585743c34501d83fde23adf34a8eee8
Yaramalla Reeja <reejayaramalla@gmail.com>
2026-03-09 15:04:27 +0530

    Add files via upload

---
+++ PSum.java
@@ -0,0 +1,15 @@
+package Vtalent.com.Feb;
+import java.util.Arrays;
+
+public class PSum {
+    public static void main(String[] args){
+        int[] arr = {24,35,30,40};
+        int key = 30;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]==key){
+                System.out.println("Element Found");
+            }
+        }
+
+    }
+}
