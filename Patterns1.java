766a2754c43af302e121e08532c3b9b0ac086ce8
Yaramalla Reeja <reejayaramalla@gmail.com>
2026-03-13 14:50:45 +0530

    Add files via upload

---
+++ Patterns1.java
@@ -0,0 +1,13 @@
+package Vtalent.com.Feb;
+
+public class Patterns1 {
+    public static void main(String[] args){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
